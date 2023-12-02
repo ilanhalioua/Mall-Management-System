062d5bdc2e2048322b89b1d162de35adec6caa1f
ilanhalioua <113393627+ilanhalioua@users.noreply.github.com>
2023-12-02 05:28:24 +0000

    Added Search product section
    Choose a product (Only distinct products appear for Products table) and select the info to be shown about that product (Can choose between: Store (or/&) Price (or/&) ProductID.
    
    When clicking 'Submit' button, a new page with the desired info appears (in json document base style).
    
    ToDo: Present data in clean table with columns: Store (or/&) Price (or/&) ProductID. (According to the selected by the user)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from database import upload_product, load_stores, load_products, upload_store, delete_store, delete_product #, load_employees_from_db
+from database import load_distinct_products, upload_product, load_stores, load_products, upload_store, mod_store, delete_store, delete_product, fetch_product_info, load_distinct_products #, mod_product, load_employees_from_db
 
 app = Flask(__name__)
 
@@ -17,6 +17,14 @@ def add_store():
     upload_store(store_name, store_area, store_status)
     return 'Store added successfully'
 
+@app.route("/edit_store", methods=['POST'])
+def edit_store():
+    store_name = request.form['store-name']
+    store_area = request.form['store-area']
+    store_status = request.form['store-status']
+    mod_store(store_name, store_area, store_status)
+    return 'Store edited successfully'
+
 @app.route("/remove_store", methods=['POST'])
 def remove_store():
     store_name = request.form['store-name']
@@ -32,12 +40,27 @@ def add_product():
     upload_product(product_name, product_price, product_store_name)
     return 'Product added successfully'
 
+# @app.route("/edit_product", methods=['POST'])
+# def edit_product():
+#     product_id = request.form['product-id']
+#     product_name = request.form['product-name']
+#     product_price = request.form['product-price']
+#     mod_product(product_id, product_name, product_price)
+#     return 'Product edited successfully'
+
 @app.route("/remove_product", methods=['POST'])
 def remove_product():
     product_id = request.form['product-id']
     delete_product(product_id)
     return 'Product removed successfully'
 
+
+@app.route("/search_product", methods=['POST'])
+def search_product():
+  product_name = request.form['product-name']
+  product_info = request.form.getlist('product-info')
+  result = fetch_product_info(product_name, product_info)
+  return jsonify(result)
 
 @app.route("/get_stores", methods=['GET'])
 def get_stores():
@@ -49,6 +72,12 @@ def get_stores():
 def get_products():
   result = load_products()
   products = [{"id": row[0], "name": row[1]} for row in result]
+  return jsonify(products)
+
+@app.route("/get_distinct_products", methods=['GET'])
+def get_distinct_products():
+  result = load_distinct_products()
+  products = [row[0] for row in result]
   return jsonify(products)
 
 
--- database.py
+++ database.py
@@ -18,6 +18,11 @@ def upload_store(store_name, store_area, store_status):
       conn.execute(text("INSERT INTO Stores (Name, Area, Status, CenterName) VALUES (:name, :area, :status, 'Plaza Norte 2')"), 
                    {"name": store_name, "area": store_area, "status": store_status})
 
+def mod_store(store_name, store_area, store_status):
+  with engine.connect() as conn:
+      conn.execute(text("UPDATE Stores SET Area = :area, Status = :status WHERE Name = :name;"), 
+                   {"name": store_name, "area": store_area, "status": store_status})
+
 def delete_store(store_name):
   with engine.connect() as conn:
     store_id_row = conn.execute(text("SELECT ID FROM Stores WHERE Name = :name"), {"name": store_name}).fetchone()
@@ -37,6 +42,11 @@ def upload_product(product_name, product_price, product_store_name):
       conn.execute(text("INSERT INTO Products (Name, Price, StoreID) VALUES (:name, :price, (SELECT ID FROM Stores WHERE Name = :store))"), 
                    {"name": product_name, "price": product_price, "store": product_store_name})
 
+# def mod_product(product_id, product_name, product_price):
+#   with engine.connect() as conn:
+#       conn.execute(text("UPDATE Products SET Name = :name, Price = :price WHERE ID = :id;"), 
+#                    {"id": product_id, "name": product_name, "price": product_price})
+
 def delete_product(product_id):
   with engine.connect() as conn:
     conn.execute(text("DELETE FROM Products WHERE ID = :id"), {"id": product_id})
@@ -48,6 +58,27 @@ def load_stores():
 def load_products():
   with engine.connect() as conn:
     return conn.execute(text("SELECT Products.ID, Products.Name FROM Products"))
+
+def load_distinct_products():
+  with engine.connect() as conn:
+    return conn.execute(text("SELECT DISTINCT Products.Name FROM Products"))
+
+def fetch_product_info(product_name, product_info):  # New function name
+  with engine.connect() as conn:
+    if "Stores" in product_info and "Price" in product_info and "Product ID" in product_info:
+      query = "SELECT Stores.Name, Products.Price, Products.ID FROM Stores JOIN Products ON Stores.ID = Products.StoreID WHERE Products.Name = :name"
+    elif "Stores" in product_info and "Price" in product_info:
+      query = "SELECT Stores.Name, Products.Price FROM Stores JOIN Products ON Stores.ID = Products.StoreID WHERE Products.Name = :name"
+    elif "Stores" in product_info and "Product ID" in product_info:
+      query = "SELECT Stores.Name, Products.ID FROM Stores JOIN Products ON Stores.ID = Products.StoreID WHERE Products.Name = :name"
+    elif "Stores" in product_info:
+      query = "SELECT Stores.Name FROM Stores JOIN Products ON Stores.ID = Products.StoreID WHERE Products.Name = :name"
+    else:
+      query = "SELECT {} FROM Products WHERE Name = :name".format(", ".join(product_info))
+    result = conn.execute(text(query), {"name": product_name})
+    return [list(row) for row in result]  # Convert each tuple into a list
+
+
 
 
 # def load_employees_from_db():
